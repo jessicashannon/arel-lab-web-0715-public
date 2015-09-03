fce4d0d59f139dc0c85386915f9b8761f7131fe4
jessicashannon <jessdahling@gmail.com>
2015-09-03 17:22:36 -0400

    taking a break before sql insanity begins

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,39 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    Boat.limit(5)
+  end
+
+  def self.dinghy
+    Boat.where("length < ?", 20)
+  end
+
+  def self.ship
+    Boat.where("length >= ?", 20)
+  end
+
+  def self.last_three_alphabetically
+    Boat.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    Boat.where(captain: nil)
+  end
+
+  def self.sailboats
+    Boat.joins(:classifications).where('classifications.name' => "Sailboat")
+  end
+
+  def self.with_three_classifications
+    
+  end
+
+
 end
+
+# 1) Boat::first_five returns the first five Boats
+#      Failure/Error: expect(Boat.first_five.pluck(:name)).to eq(boats)
+#      NoMethodError:
+#        undefined method `first_five' for #<Class:0x007ffb6d550198>
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,8 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    Captain.joins(:boats :boat_classifications :classifications).where('classifications.name' => 'Catamaran')
+  end
+
 end
